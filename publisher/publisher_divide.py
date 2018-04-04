df91d20340ff063b9933bb81ecc1a13b36aa8194
leiquanpan <lpan22@wustl.edu>
2018-04-03 20:52:06 -0500

    image division

---
+++ publisher/publisher_divide.py
@@ -0,0 +1,143 @@
+'''
+/*
+ * Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
+import cv2
+import random
+
+AllowedActions = ['both', 'publish', 'subscribe']
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
+                    help="Use MQTT over WebSocket")
+parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicPubSub",
+                    help="Targeted client id")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+parser.add_argument("-m", "--mode", action="store", dest="mode", default="both",
+                    help="Operation modes: %s"%str(AllowedActions))
+parser.add_argument("-M", "--message", action="store", dest="message", default="Hello World!",
+                    help="Message to publish")
+parser.add_argument("-i", "--interval", action="store", dest="interval", default="2",
+                    help="waiting for publish to finish")
+
+args = parser.parse_args()
+host = args.host
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+useWebsocket = args.useWebsocket
+clientId = args.clientId
+topic = args.topic
+interval = args.interval
+
+if args.mode not in AllowedActions:
+    parser.error("Unknown --mode option %s. Must be one of %s" % (args.mode, str(AllowedActions)))
+    exit(2)
+
+if args.useWebsocket and args.certificatePath and args.privateKeyPath:
+    parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
+    exit(2)
+
+if not args.useWebsocket and (not args.certificatePath or not args.privateKeyPath):
+    parser.error("Missing credentials for authentication.")
+    exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+if useWebsocket:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
+    myAWSIoTMQTTClient.configureEndpoint(host, 443)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+else:
+    myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+    myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+    myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+
+myAWSIoTMQTTClient.connect()
+if args.mode == 'both' or args.mode == 'subscribe':
+    myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+while loopCount < 1:
+    if args.mode == 'both' or args.mode == 'publish':
+        message = {}
+	path = args.message
+	img = cv2.imread(path,1)
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),100]
+        result, encimg = cv2.imencode('.jpg', img, encode_param)
+        #add partition
+        original_graph = bytearray(encimg)
+        length = len(original_graph)
+        
+        first_part = original_graph[:length/2]
+        second_part = original_graph[length/2:length]
+        
+        #add identification
+        first_part.insert(0,(0).to_bytes(1, byteorder='big'))
+        second_part.insert(0,(1).to_bytes(1, byteorder='big'))
+        
+        #add random number
+        random_number = random.randint(1,1024)
+        first_part.append(random_number.to_bytes(2, byteorder='big'))
+        second_part.append(random_number.to_bytes(2, byteorder='big'))
+        
+        myAWSIoTMQTTClient.publish(topic, first_part, 1)
+        print("Published first half of message " + loopCount)
+        myAWSIoTMQTTClient.publish(topic, second_part, 1)
+        print("Published second half of message " + loopCount)
+        
+        loopCount += 1
+    time.sleep(float(interval))
+
+print("Finished Publishing")
---
+++ subscriber/subscriber/subscriber_divided.py
@@ -0,0 +1,104 @@
+# Author: lpan22@wustl.edu
+# This file is for reveicing image from AWS IoT and store them into local file system, and forward them to Face-recognition file to get the results
+
+import paho.mqtt.client as paho
+import os
+import socket
+import ssl
+import json
+from time import sleep
+from random import randint
+from datetime import datetime
+import argparse
+import cv2
+import numpy as np
+import time
+import subprocess
+
+#initialize hash_map
+print("Initialize dictions")
+hash_map = {}
+
+
+#processing first part of image, insert into hash_map
+def process_first_part(random_number, image_data):
+    hash_map[random_number] = image_data
+
+
+#processing second part of image, if match generate
+def process_second_part(random_number, image_data):
+    if(!hash_map.has_key(random_number)):
+        print("There is no first map matching, drop it")
+        return
+    
+    first_half = hash_map[random_number]
+    hash_map.pop(random_number)
+    whole_image = first_half + image_data
+
+    #begin to grnerate image
+    timestamp = str(time.time())
+    filePath = os.getcwd() + "/../pictures/received_picture_" + timestamp + ".jpeg"
+    image = np.asarray(whole_image,dtype="uint8")
+    decimg = cv2.imdecode(image, 1)
+    print("Received a photo, and stored time into %s" % filePath)
+    print("start processing")
+    command = "python3 ../face_rocoginition/faceCount.py -f " + filePath + " -e ../processed_pictures"
+    subprocess.call(command,shell = True)
+    print("end processing")
+
+
+#connect callback
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: " + str(rc))
+
+
+#new_function of message callback
+def on_message(client, userdata, msg):
+    #start to analysis data,analyze ID,random_id,msg_data
+    input_msg = bytearray(msg.payload)
+    length = len(input_msg)
+    
+    identity = int.from_bytes(input_msg[0],byteorder='big')
+    random_id = int.from_bytes(input_msg[length-2:length],byteorder='big')
+    half_image_data = input_msg[1:length-2]
+    
+    if (identity == 0):
+        process_first_part(random_id, half_image_data)
+    else if (identity == 1):
+        process_second_part(random_id, half_image_data)
+
+    print("End one receive callback")
+
+
+# Read in command-line parameters
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--endpoint", action="store", required=True, dest="awsEndpoint", help="Your AWS IoT custom endpoint")
+parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
+parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
+parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
+parser.add_argument("-t", "--topic", action="store", dest="topic", default="sdk/test/Python", help="Targeted topic")
+
+args = parser.parse_args()
+awsEndpoint = args.awsEndpoint
+rootCAPath = args.rootCAPath
+certificatePath = args.certificatePath
+privateKeyPath = args.privateKeyPath
+topic = args.topic
+
+# Create an mqtt Client Object: mqttc
+# Bind the callback method on specific event
+mqttc = paho.Client()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+awsPort     = 8883
+qos         = 0
+
+mqttc.tls_set(rootCAPath, certfile = certificatePath, keyfile = privateKeyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+
+mqttc.connect(awsEndpoint, awsPort, keepalive=60)
+mqttc.subscribe(topic , qos)
+mqttc.loop_start()
+
+while 1==1:
+    sleep(2)
