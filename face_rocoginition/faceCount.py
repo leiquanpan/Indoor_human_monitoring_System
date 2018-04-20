f5e82166db4c633b119053886a81f4998d9c181a
leiquanpan <lpan22@wustl.edu>
2018-04-20 13:39:18 -0500

    Combined together

--- face_rocoginition/faceCount.py
+++ face_rocoginition/faceCount.py
@@ -23,6 +23,11 @@ endPath = args.endPath
 # Load the jpg file into a numpy array
 image = face_recognition.load_image_file(filePath)
 
+#initialize matrix
+w, h = 3, 4;
+Matrix = [[0 for x in range(w)] for y in range(h)]
+
+
 # Find all the faces in the image using the default HOG-based model.
 # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
 # See also: find_faces_in_picture_cnn.py
@@ -34,7 +39,34 @@ for face_location in face_locations:
     cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
     font = cv2.FONT_HERSHEY_DUPLEX
     cv2.putText(image, "FACE", (left + 6, bottom - 6), font, 1, (255, 255, 255), 1)
-text = "There is " + str(len(face_locations)) + " human faces"
+    
+    mid_point_x=left+(right-left)/2
+    mid_point_y=top+(bottom-top)/2
+
+    if mid_point_x>=442 and mid_point_x<=617 and mid_point_y>=541 and mid_point_y<=671:
+        Matrix[0][0]=1;
+    if mid_point_x>=508 and mid_point_x<=693 and mid_point_y>=455 and mid_point_y<=538:
+        Matrix[1][0]=1;
+    if mid_point_x>=584 and mid_point_x<=735 and mid_point_y>=428 and mid_point_y<=507:
+        Matrix[2][0]=1;
+    if mid_point_x>=658 and mid_point_x<=816 and mid_point_y>=383 and mid_point_y<=464:
+        Matrix[3][0]=1;
+    if mid_point_x>=998 and mid_point_x<=1212 and mid_point_y>=527 and mid_point_y<=669:
+        Matrix[0][1]=1;
+    if mid_point_x>=992 and mid_point_x<=1184 and mid_point_y>=410 and mid_point_y<=521:
+        Matrix[1][1]=1;
+    if mid_point_x>=1000 and mid_point_x<=1159 and mid_point_y>=455 and mid_point_y<=541:
+        Matrix[2][1]=1;
+    if mid_point_x>=1013 and mid_point_x<=1149 and mid_point_y>=377 and mid_point_y<=450:
+        Matrix[3][1]=1;
+    if mid_point_x>=1556 and mid_point_x<=1704 and mid_point_y>=522 and mid_point_y<=623:
+        Matrix[0][2]=1;
+    if mid_point_x>=1432 and mid_point_x<=1584 and mid_point_y>=430 and mid_point_y<=522:
+        Matrix[1][2]=1;
+    if mid_point_x>=1385 and mid_point_x<=1519 and mid_point_y>=422 and mid_point_y<=493:
+        Matrix[2][2]=1;
+    if mid_point_x>=1318 and mid_point_x<=1459 and mid_point_y>=381 and mid_point_y<=449:
+        Matrix[3][2]=1;
 
 #cv2.putText(frame, text, (), font, 1, (255, 255, 255), 1)
 #store the photo in the endPath
@@ -42,5 +74,5 @@ time = str(datetime.now())
 outputPath = endPath + "/processed_picture_" + time + ".jpg"
 cv2.imwrite(outputPath,image)
 
-print("There is {} face(s).".format(len(face_locations)))
+#you need to put matrix into database.
 
